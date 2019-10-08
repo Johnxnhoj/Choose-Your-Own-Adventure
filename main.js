43f5c71cdad02f7b9aab507c902c0691aa13b79e
Johnxnhoj <55994152+Johnxnhoj@users.noreply.github.com>
2019-10-08 08:44:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+alert("it is friday the 13th and jason is running after you try to figure out your way out! ready go.")
+var yesno= confirm("You are inside of the cabin in the middle of the woods choose no to go outside the cabin or yes to stay inside!!")
+console.log(yesno).
+
+if (false){alert("you went out the front door and got your leg cuaght in a bear trapp, jason came and cut your head off.GAME OVER!")}//end game
+
+if (true){alert("good idea, hes outside waiting for you to fall for his trapps.")}//on to the next
+
+
+
+
+var yesno=confirm("Now you choose if you want to go to the basment'NO' or choose to go to the attic 'YES'")
+
+
+
+
+
+
+
+
+
+
+
+
